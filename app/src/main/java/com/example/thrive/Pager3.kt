afd7d5a32ce75e5c712f4f59a7d650e3769b54a9
adahaggarwal <adahaggarwal6@gmail.com>
2024-09-07 19:09:08 +0530

    errors

--- app/src/main/java/com/example/thrive/Pager3.kt
+++ app/src/main/java/com/example/thrive/Pager3.kt
@@ -11,10 +11,6 @@ class Pager3 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.pager3)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
