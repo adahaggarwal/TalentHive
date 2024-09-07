b38c6f6b35e4bafd92ef7e677f4a62ef4254099f
jdesai05 <jainildesai05@gmail.com>
2024-09-07 20:04:41 +0530

    I

---
+++ app/src/main/java/com/example/thrive/Pager1.kt
@@ -0,0 +1,14 @@
+package com.example.thrive
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class Pager1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.pager2)
+
+    }
+}
