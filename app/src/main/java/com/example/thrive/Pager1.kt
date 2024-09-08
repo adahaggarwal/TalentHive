2b682913ce0a6bc325c8ccccd912e7a87d7d44ba
adahaggarwal <adahaggarwal6@gmail.com>
2024-09-08 16:49:46 +0530

    errors

--- app/src/main/java/com/example/thrive/Pager1.kt
+++ app/src/main/java/com/example/thrive/Pager1.kt
@@ -3,24 +3,24 @@ package com.example.thrive
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AppCompatActivity
 import com.example.thrive.R.*
 
 class Pager1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(layout.pager1)
 
 
-        val next1 = findViewById<TextView>(R.id.n1)
-        next1.setOnClickListener(){
-            val intent = Intent(this, Pager2::class.java)
-            startActivity(intent)
-
-
-        }
+//        val next1 = findViewById<TextView>(R.id.nex)
+//        next1.setOnClickListener(){
+//            val intent = Intent(this, Pager2::class.java)
+//            startActivity(intent)
+//
+//
+//        }
 
     }
 }
--- app/src/main/java/com/example/thrive/Pager2.kt
+++ app/src/main/java/com/example/thrive/Pager2.kt
@@ -1,6 +1,8 @@
 package com.example.thrive
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,6 +11,14 @@ class Pager2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.pager2)
+
+        val next2 = findViewById<TextView>(R.id.next2)
+        next2.setOnClickListener(){
+            val intent = Intent(this, Pager3::class.java)
+            startActivity(intent)
+
+
+        }
 
     }
 }
