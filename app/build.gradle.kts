88feff355e61cf8917aa39a6b343e93a140d3bad
adahaggarwal <adahaggarwal6@gmail.com>
2024-09-08 14:55:43 +0530

    errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation (libs.circleprogress)
+
 
     implementation (libs.glide)
     annotationProcessor (libs.compiler)
--- app/src/main/java/com/example/thrive/Profile.kt
+++ app/src/main/java/com/example/thrive/Profile.kt
@@ -1,6 +1,8 @@
 package com.example.thrive
 
 import android.os.Bundle
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,11 @@ class Profile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val progressText = findViewById<TextView>(R.id.progressText)
+
+        progressBar.progress = 72 // set the progress
+        progressText.text = "${progressBar.progress}%"
     }
 }
