2d95683b66542aebd5c9076aa71bafae56ca0016
adahaggarwal <adahaggarwal6@gmail.com>
2024-09-09 22:43:08 +0530

    corrected pagers

--- app/src/main/java/com/example/thrive/Termsandcon.kt
+++ app/src/main/java/com/example/thrive/Termsandcon.kt
@@ -1,6 +1,10 @@
 package com.example.thrive
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ScrollView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +16,26 @@ class Termsandcon : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_termsandcon)
 
+        val ag = findViewById<Button>(R.id.ag)
+        ag.setOnClickListener(){
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+
+        val scrollView  = findViewById<ScrollView>(R.id.sc)
+        val scrollToTopButton = findViewById <Button>(R.id.scrollBtn)
+
+        scrollToTopButton.setOnClickListener {
+            scrollView.smoothScrollTo(0, 0)
+        }
+
+        scrollView.viewTreeObserver.addOnScrollChangedListener {
+            val scrollY = scrollView.scrollY
+            scrollToTopButton.visibility = if (scrollY > 100) {
+                Button.VISIBLE
+            } else {
+                Button.GONE
+            }
+        }
     }
 }
