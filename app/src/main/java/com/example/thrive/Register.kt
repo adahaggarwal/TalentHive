db919e7d1a97c5e37e70bab83082572aedf19b6d
adahaggarwal <adahaggarwal6@gmail.com>
2024-09-09 22:48:47 +0530

    corrected pagers

--- app/src/main/java/com/example/thrive/Register.kt
+++ app/src/main/java/com/example/thrive/Register.kt
@@ -1,6 +1,9 @@
 package com.example.thrive
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
@@ -11,6 +14,12 @@ class Register : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
+
+        val cracc = findViewById<LinearLayout>(R.id.cracc)
+        cracc.setOnClickListener(){
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
 
     }
 }
