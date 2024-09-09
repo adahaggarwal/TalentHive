a45038ccddabc6315cd262135a2b7d53352a7d6e
adahaggarwal <adahaggarwal6@gmail.com>
2024-09-09 23:01:37 +0530

    corrected pagers

--- app/src/main/java/com/example/thrive/Login.kt
+++ app/src/main/java/com/example/thrive/Login.kt
@@ -1,6 +1,9 @@
 package com.example.thrive
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,23 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val ftgpass = findViewById<TextView>(R.id.ftgpass)
+        ftgpass.setOnClickListener(){
+            val intent = Intent(this, ForgotPass::class.java)
+            startActivity(intent)
+        }
+
+        val log = findViewById<LinearLayout>(R.id.log)
+        log.setOnClickListener(){
+            val intent = Intent(this, Profile::class.java)
+            startActivity(intent)
+        }
+
+        val si = findViewById<TextView>(R.id.si)
+        si.setOnClickListener(){
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
         }
     }
 }
