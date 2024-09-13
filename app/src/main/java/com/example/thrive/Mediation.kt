d43c0cb51259dbfa6aa2502b9f31d37a4655c6d2
adahaggarwal <adahaggarwal6@gmail.com>
2024-09-13 09:02:28 +0530

    regi

--- app/src/main/java/com/example/thrive/Mediation.kt
+++ app/src/main/java/com/example/thrive/Mediation.kt
@@ -63,6 +63,8 @@ class Mediation : Fragment() {
         fragmentTransaction.commit()
     }
 
+
+
     companion object {
         /**
          * Use this factory method to create a new instance of
--- app/src/main/java/com/example/thrive/chatjason.kt
+++ app/src/main/java/com/example/thrive/chatjason.kt
@@ -5,11 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Messaging.newInstance] factory method to
+ * Use the [chatjason.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Messaging : Fragment() {
+class chatjason : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -39,24 +34,7 @@ class Messaging : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_messaging, container, false)
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val j = view.findViewById<TextView>(R.id.j)
-        j.setOnClickListener(){
-            replaceFragment(chatjason())
-        }
-
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager: FragmentManager = parentFragmentManager
-        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment_container, fragment)
-        fragmentTransaction.addToBackStack(null)
-        fragmentTransaction.commit()
+        return inflater.inflate(R.layout.fragment_chatjason, container, false)
     }
 
     companion object {
@@ -66,12 +44,12 @@ class Messaging : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Messaging.
+         * @return A new instance of fragment chatjason.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Messaging().apply {
+            chatjason().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
